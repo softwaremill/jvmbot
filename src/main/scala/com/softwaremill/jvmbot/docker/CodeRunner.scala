a8c77548f8f77a66259a1ab28d0d720b27a07ba2
Piotr Buda <pibuda@gmail.com>
2014-11-28 14:08:08 +0100

    Adds code runner that invokes docker and runs provided code

--- src/main/scala/com/softwaremill/jvmbot/docker/CodeRunner.scala
+++ src/main/scala/com/softwaremill/jvmbot/docker/CodeRunner.scala
@@ -1,11 +1,13 @@
 package com.softwaremill.jvmbot.docker
 
 import akka.actor.Actor
+import com.softwaremill.jvmbot.docker.DockerRunner
 
 class CodeRunner extends Actor {
+  val Image = "webratio/groovy:2.3.7"
+  val runner = new DockerRunner(Image)
   override def receive = {
     case code: String =>
-      // TODO: run docker and send back code result
-      sender() ! code
+      sender() ! runner.run(code)
   }
 }
--- src/main/scala/com/softwaremill/jvmbot/docker/DockerRunner.scala
+++ src/main/scala/com/softwaremill/jvmbot/docker/DockerRunner.scala
@@ -4,18 +4,23 @@ import com.spotify.docker.client.DefaultDockerClient
 import com.spotify.docker.client.DockerClient.LogsParameter
 import com.spotify.docker.client.messages.ContainerConfig
 
-object DockerRunner extends App {
-  val docker = DefaultDockerClient.fromEnv().build()
-  docker.pull("java:7")
-  val config = ContainerConfig.builder().image("java:7").cmd("sh", "-c", "while :; do echo dupa; sleep 1; done").attachStderr(true).attachStdout(true).build()
-  val creation = docker.createContainer(config)
-  val id = creation.id()
-  docker.startContainer(id)
-  Thread.sleep(60000)
-  docker.killContainer(id)
-  val logs = docker.logs(id, LogsParameter.STDOUT)
-  println(logs.readFully())
-  logs.close()
-  docker.removeContainer(id)
-  docker.close()
+class DockerRunner(image:String) {
+  def run(code:String) = {
+    val docker = DefaultDockerClient.fromEnv().build()
+    docker.pull(image)
+    val config = ContainerConfig.builder().image(image)
+      .cmd(s"-e println $code").attachStderr(true).attachStdout(true).build()
+    val creation = docker.createContainer(config)
+    val id = creation.id()
+    docker.startContainer(id)
+    Thread.sleep(3000L)
+    docker.killContainer(id)
+    val logs = docker.logs(id, LogsParameter.STDERR, LogsParameter.STDOUT)
+    val log = logs.readFully()
+    println("end")
+    logs.close()
+    docker.removeContainer(id)
+    docker.close()
+    log
+  }
 }
