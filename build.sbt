383ada02618d988f9a263a86b323a88f6fd0d5be
szimano <szimano@szimano.org>
2014-11-28 13:06:55 +0100

    send tweets via SQS

--- build.sbt
+++ build.sbt
@@ -11,6 +11,7 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.1.3" % "test",
   "org.twitter4j" % "twitter4j-core" % "4.0.2",
   "com.typesafe.akka" %% "akka-actor" % "2.3.7",
-  "com.amazonaws" % "aws-java-sdk" % "1.9.8"
-  "com.spotify" % "docker-client" % "2.7.1"
+  "com.amazonaws" % "aws-java-sdk" % "1.9.8",
+  "com.spotify" % "docker-client" % "2.7.1",
+  "org.scala-lang" %% "scala-pickling" % "0.9.1"
 )
--- src/main/scala/com/softwaremill/jvmbot/TwitterClient.scala
+++ src/main/scala/com/softwaremill/jvmbot/TwitterClient.scala
@@ -5,17 +5,24 @@ import com.amazonaws.auth.{PropertiesCredentials, BasicAWSCredentials, AWSCreden
 import com.amazonaws.regions.{Region, Regions}
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.model.{ScanRequest, AttributeValue, PutItemRequest, BatchGetItemRequest}
+import com.amazonaws.services.sqs.AmazonSQSClient
+import com.amazonaws.services.sqs.model.Message
 import twitter4j._
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 import scala.concurrent.duration._
+import scala.pickling._
+import scala.pickling.json._
 
 object TwitterClient extends App {
   val actorSystem = ActorSystem()
   import actorSystem.dispatcher
-  val mentionConsumer = actorSystem.actorOf(Props(classOf[MentionConsumer]))
+  val queueSender = actorSystem.actorOf(Props(classOf[MentionQueueSender]))
+  val mentionConsumer = actorSystem.actorOf(Props(classOf[MentionConsumer], queueSender))
   val mentionPuller = actorSystem.actorOf(Props(classOf[MentionPuller], mentionConsumer))
+  val queuePuller = actorSystem.actorOf(Props(classOf[MentionQueueReceiver]))
   actorSystem.scheduler.schedule(0.seconds, 1.minute, mentionPuller, Pull)
+  actorSystem.scheduler.schedule(0.seconds, 5.seconds, queuePuller, Pull)
 }
 
 class MentionPuller(consumer: ActorRef) extends Actor {
@@ -32,16 +39,17 @@ class MentionPuller(consumer: ActorRef) extends Actor {
   }
 }
 
-class MentionConsumer extends Actor {
+class MentionConsumer(queueSender: ActorRef) extends Actor {
+  import AWS._
+
   var consumedMentions: Set[Long] = Set()
-  val client = new AmazonDynamoDBClient(new PropertiesCredentials(this.getClass.getResourceAsStream("/aws.properties")))
 
   val DynamoTable = "jvmbot"
   val DynamoId = "tweet_id"
 
   override def preStart() = {
-    client.setRegion(Region.getRegion(Regions.US_EAST_1))
-    val items = client.scan(new ScanRequest(DynamoTable))
+    dynamoClient.setRegion(Region.getRegion(Regions.US_EAST_1))
+    val items = dynamoClient.scan(new ScanRequest(DynamoTable))
     items.getItems.foreach(item => consumedMentions += item.get(DynamoId).getS.toLong)
     println(s"Loaded tweets from dynamo, size: ${consumedMentions.size}")
   }
@@ -49,12 +57,45 @@ class MentionConsumer extends Actor {
   override def receive = {
     case s:Status =>
       if (!consumedMentions.contains(s.getId)) {   
-        client.putItem(new PutItemRequest(DynamoTable, 
+        dynamoClient.putItem(new PutItemRequest(DynamoTable,
           mapAsJavaMap(Map(DynamoId -> new AttributeValue().withS(s.getId.toString)))))
         println(s"Wrote status with id ${s.getId} to dynamo")
         consumedMentions += s.getId
+        queueSender ! s
+      }
+  }
+}
+
+class MentionQueueSender extends Actor {
+  import AWS._
+
+  override def receive = {
+    case s:Status =>
+      sqsClient.sendMessage(queueUrl, JsonStatus(s.getText, s.getUser.getScreenName).pickle.value)
+  }
+}
+
+class MentionQueueReceiver extends Actor {
+  import AWS._
+
+  override def receive = {
+    case Pull =>
+      sqsClient.receiveMessage(queueUrl).getMessages.foreach { message: Message =>
+        sqsClient.deleteMessage(queueUrl, message.getReceiptHandle)
+        val status = message.getBody.unpickle[JsonStatus]
+        println(s"got this awesome status: ${status}")
       }
   }
 }
 
 object Pull
+
+object AWS {
+  val awsCredentials: PropertiesCredentials = new PropertiesCredentials(this.getClass.getResourceAsStream("/aws.properties"))
+  val dynamoClient = new AmazonDynamoDBClient(awsCredentials)
+  val sqsClient = new AmazonSQSClient(awsCredentials)
+  val QueueName = "jvmbot"
+  val queueUrl = sqsClient.createQueue(QueueName).getQueueUrl
+}
+
+case class JsonStatus(msg: String, source: String)
